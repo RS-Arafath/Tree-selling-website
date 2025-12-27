01d61e273a85399a5152a4349555eff08456afc5
Arafath <rsarafath242@gmail.com>
2025-12-27 20:56:16 +0600

    i have fixed some issu

--- script.js
+++
@@ -1,9 +0,0 @@
-// ---toogle button
-const navMenu = document.getElementById('nav-menu');
-const navLink = document.querySelectorAll('.nav-link');
-const hamburger = document.getElementById('hamburger');
-
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('left-[0]');
-  hamburger.classList.toggle('ri-close-large-line');
-});
